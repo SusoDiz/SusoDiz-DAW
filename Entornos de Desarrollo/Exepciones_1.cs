6c5b1b13418057319db41075a9e4e65dfcded89d
SusoDiz <jesusgonzalezdiaz77@gmail.com>
2023-11-27 23:53:53 +0000

    Contenidos Iniciales

---
+++ Entornos de Desarrollo/Exepciones_1.cs
@@ -0,0 +1,28 @@
+﻿namespace ETS_exepciones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 19;
+            int numero = 0;
+
+            try
+            {
+                Console.WriteLine(edad / numero);
+            }
+            catch
+            {
+                Console.WriteLine("¡¡¡Hay un error por aqui!!!");
+                Console.WriteLine("Pulsa ENTER para salir... ");
+                Console.ReadKey();
+                return;
+            }
+            finally
+            {
+                Console.WriteLine("Edad: " + edad);
+            }
+
+        }
+    }
+}
